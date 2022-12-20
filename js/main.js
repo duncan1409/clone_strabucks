3326a995b9d921c1786b77ea9f52eb19a77eda03
duncan1409 <duncansITworld@gmail.com>
2022-12-20 21:34:57 +0900

    swiper

--- js/main.js
+++ js/main.js
@@ -20,15 +20,15 @@ const badgeEl = document.querySelector('header .badges');
 
 //_.throttle(함수, 시간)
 //gsap
-window.addEventListener('scroll', _.throttle(function(){
+window.addEventListener('scroll', _.throttle(function () {
 	console.log(window.scrollY);
-	if(window.scrollY>500){
+	if (window.scrollY > 500) {
 		//Badge 숨기기
 		gsap.to(badgeEl, .3, {
 			opacity: 0,
 			display: 'none'
 		});
-	}else{
+	} else {
 		//Badge 보이기
 		gsap.to(badgeEl, .3, {
 			opacity: 1,
@@ -36,3 +36,20 @@ window.addEventListener('scroll', _.throttle(function(){
 		});
 	}
 }, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+	gsap.to(fadeEl, 1, {
+		delay: (index + 1) * .7, //순차적으로 보여주는 것에 주의... 0.7, 1.4, 2.1...
+		opacity: 1
+	});
+})
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+	direction: 'vertical', // 수직 슬라이드
+	autoplay: true, // 자동재생
+	loop: true //반복재생
+});
