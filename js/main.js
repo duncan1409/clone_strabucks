8168726255ab2afc4f3ed1222fc4688aa3411506
duncan1409 <duncansITworld@gmail.com>
2022-12-22 16:55:06 +0900

    20221222

--- js/main.js
+++ js/main.js
@@ -62,12 +62,29 @@ new Swiper('.promotion .swiper-container', {
 	autoplay: {
 		delay: 5000
 	},
-	pagination:{
+	pagination: {
 		el: '.promotion .swiper-pagination', //페이지 번호 요소의 선택자.
 		clickable: true //사용자의 페이지 번호 요소 제어
 	},
-	navigation:{
-		prevEl:'.promotion .swiper-prev',
-		nextEl:'.promotion .swiper-next'
+	navigation: {
+		prevEl: '.promotion .swiper-prev',
+		nextEl: '.promotion .swiper-next'
+	}
+});
+
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+	isHidePromotion = !isHidePromotion;
+	if (isHidePromotion) {
+		//숨김처리!
+		promotionEl.classList.add('hide');
+	} else {
+		//보임처리!
+		promotionEl.classList.remove('hide');
+	
 	}
 });
